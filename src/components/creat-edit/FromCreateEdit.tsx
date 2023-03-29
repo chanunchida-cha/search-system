d201cc85b5b8b2cd44b81639a44f60293e1bad77
Landmark <se56160381@gmail.com>
2023-03-29 15:23:19 +0700

    Tab Form

--- src/components/creat-edit/FromCreateEdit.tsx
+++ src/components/creat-edit/FromCreateEdit.tsx
@@ -1,15 +1,41 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from "react";
+import HistoryForm from "./historyForm/HistoryForm";
+import AssessmentForm from "./assessmentForm/AssessmentForm";
 
-interface Props {
-    
-}
+interface Props {}
+
+const typeTab = [
+  {
+    type: "history",
+    i18n: "ประวัติผู้ทรงคุณวุฒิ",
+  },
+  {
+    type: "Assessment",
+    i18n: "ข้อมูลผลการประเมิน",
+  },
+];
 
 function FromCreateEdit({}: Props): ReactElement {
-    return (
-        <div>
-            
-        </div>
-    )
+const [type, settype] = useState("");
+  return (
+    <div>
+      <div className=" grid grid-cols-10 gap-4">
+        {typeTab.map((data) => {
+          return (
+            <div
+              className="flex justify-center bg-blue-400"
+              onClick={() => {
+                settype(data.type);
+              }}
+            >
+              {data.i18n}
+            </div>
+          );
+        })}
+      </div>
+      <div>{type === "history" ? <HistoryForm/> : <AssessmentForm/>}</div>
+    </div>
+  );
 }
 
-export default FromCreateEdit
+export default FromCreateEdit;

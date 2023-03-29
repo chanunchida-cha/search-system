a4dc1c0ffc2e080886364b029244cc8d25d257c7
gulnwaot55 <gulnwaot55>
2023-03-29 11:03:09 +0700

    update login screen

--- src/components/login/LoginMain.tsx
+++ src/components/login/LoginMain.tsx
@@ -1,24 +1,24 @@
+/* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState } from "react";
 
 type Props = {};
 
 function loginMain({}: Props) {
-
   const [userName, setUserName] = useState("");
   const [userPassword, setUserPassword] = useState("");
 
   return (
-    <div className="flex items-center justify-center bg-gray-100 p-64">
-      <div className="rounded-lg bg-white p-2 shadow lg:w-[28rem] lg:h-[26rem]">
-        <div className="mt-2 text-center p-4">
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <div className="rounded-lg bg-white p-2 shadow lg:h-[26rem] lg:w-[28rem]">
+        <div className="mt-2 p-4 text-center">
           <p className="text-2xl font-bold text-blue-800">System Name</p>
         </div>
 
         <form className="ml-5 mr-5 mt-5">
           <div className="mb-6">
             <label
-              form="email"
+              htmlFor="email"
               className="mb-2 block text-sm font-bold text-blue-800"
             >
               Email
@@ -29,15 +29,16 @@ function loginMain({}: Props) {
               className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
               placeholder="plengSlowtoad@bluebik.vulcan.com"
               onChange={(event) => {
-                setUserName(event.target.value)
-                console.log(event.target.value)
+                setUserName(event.target.value);
+                console.log(event.target.value);
               }}
+              value={userName}
               required
             ></input>
           </div>
           <div className="mb-6 pb-8">
             <label
-              form="password"
+              htmlFor="password"
               className="mb-2 block text-sm font-bold text-blue-800"
             >
               Password
@@ -48,21 +49,22 @@ function loginMain({}: Props) {
               className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 "
               placeholder="•••••••••••"
               onChange={(event) => {
-                setUserPassword(event.target.value)
-                console.log(event.target.value)
+                setUserPassword(event.target.value);
+                console.log(event.target.value);
               }}
+              value={userPassword}
               required
             ></input>
           </div>
           <div className="mb-6 flex items-center justify-center">
-          <div className="flex h-5 items-center">
-          <button
-            type="submit"
-            className="mb-4  rounded-full bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
-          >
-            Sign In
-          </button>
-          </div>
+            <div className="flex h-5 items-center">
+              <button
+                type="submit"
+                className="mb-4  rounded-full bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
+              >
+                Sign In
+              </button>
+            </div>
           </div>
         </form>
       </div>

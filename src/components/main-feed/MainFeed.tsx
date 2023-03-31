442149d992954a48c51459df82c1aa30e6d8f087
Nattakorn <nonmom2550@gmail.com>
2023-03-31 10:23:13 +0700

    Call SideBar to MainFeed and Fixed ResponsiveUI

--- src/components/main-feed/MainFeed.tsx
+++ src/components/main-feed/MainFeed.tsx
@@ -12,23 +12,31 @@ function MainFeed({}: Props) {
     <>
       <div className="align-center h-screen bg-gray-100 p-5">
         {/* Heading and AddUserData */}
-        <div className="mb-2 grid h-12 grid-cols-2">
+        <div className="mb-2 grid h-12 grid-cols-1 md:grid md:grid-cols-2">
           <div className="flex items-center justify-start">
-            <h1 className="sm:text-2 md:text-xl lg:text-2xl">
+            <h1 className="sm:text-2 text-black md:text-xl lg:text-2xl">
               ข้อมูลรายชื่อผู้วิจัย
             </h1>
           </div>
-          <div className="flex items-center justify-end ">
-            <div className="mt-2  w-auto  md:w-3/5  lg:w-3/5 xl:w-2/5 ">
+          <div className="flex items-center justify-start md:justify-end ">
+            <div className="mt-2  w-auto md:w-full lg:w-4/5 xl:w-3/5">
               <button className="flex w-full items-center justify-center rounded-lg bg-[#0265ff] p-1">
-                <Image
-                  src="/assets/images/icon-add.png"
-                  width={24}
-                  height={24}
-                  alt="add"
-                  className=""
-                />
-                <p className=" ml-1 text-xs text-white md:text-lg lg:text-xl">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  className="h-5 w-5"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                  ></path>
+                </svg>
+                <p className=" ml-1 text-xs text-white md:text-xs lg:text-lg ">
                   เพิ่มข้อมูลผู้ทรงคุณวุฒิ
                 </p>
               </button>
@@ -38,7 +46,7 @@ function MainFeed({}: Props) {
         {/* END OF Heading and AddUserData */}
 
         {/* SearchBar and TypeSelection */}
-        <div className="mb-3 grid h-12">
+        <div className="mb-3 mt-5 grid h-12 md:mt-0">
           <div className="flex items-center justify-end">
             {/* SearchBar */}
             <div className="relative w-2/3 md:w-2/5 lg:w-2/5">
@@ -139,20 +147,38 @@ function MainFeed({}: Props) {
                 <td className="whitespace-nowrap p-3 text-center text-sm text-gray-700">
                   <div className="flex flex-col place-items-center gap-2 md:flex-row lg:flex-row">
                     <button className="rounded bg-[#f1d386] py-2 px-4 font-bold text-white hover:bg-yellow-700">
-                      <Image
-                        src="/assets/images/icon-update.png"
-                        width={24}
-                        height={24}
-                        alt="update"
-                      />
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="1.5"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true"
+                        className="h-5 w-5"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                        ></path>
+                      </svg>
                     </button>
                     <button className="rounded bg-[#ec5e43] py-2 px-4 font-bold text-white hover:bg-yellow-700">
-                      <Image
-                        src="/assets/images/icon-delete.png"
-                        width={24}
-                        height={24}
-                        alt="delete"
-                      />
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="1.5"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true"
+                        className="h-5 w-5"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        ></path>
+                      </svg>
                     </button>
                   </div>
                 </td>

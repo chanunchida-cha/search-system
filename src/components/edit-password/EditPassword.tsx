2be5857300fd883028df37a8b37768dac3aeb3df
Thossapol Seekram <gulnwaot55>
2023-04-25 14:09:13 +0700

    Responsive EditPass Page

--- src/components/edit-password/EditPassword.tsx
+++ src/components/edit-password/EditPassword.tsx
@@ -21,44 +21,46 @@ function EditPassword({ }: Props) {
         <div className="flex items-center justify-center h-screen bg-white rounded-lg">
             <div className="w-11/12 md:w-1/2 lg:w-1/3">
                 
-                <form className="bg-gray-100 shadow-md rounded-lg px-8 pt-20 pb-20 pl-20 pr-20" onSubmit={handleSubmit}>
-                    <div className="mb-2 pl-10 pr-10">
+                <form className="bg-gray-100 shadow-md rounded-lg px-8 pt-20 pb-20 lg:pl-20 lg:pr-20" onSubmit={handleSubmit}>
+                    <div className="mb-4 lg:mb-2">
                         <label className="block text-gray-700 font-bold mb-2">
                             ชื่อผู้ใช้งาน
                         </label>
                         <input
-                            className="pointer-events-none shadow rounded-lg appearance-none border  w-full py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
+                            className="pointer-events-none shadow rounded-lg appearance-none border w-full py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
                             placeholder="Suchart Thongyod"
                         />
                     </div>
-                    <div className="mb-2 pl-10 pr-10">
+                    <div className="mb-4 lg:mb-2">
                         <label className="block text-gray-700 font-bold mb-2">
                             รหัสผ่านเก่า
                         </label>
                         <input
                             className="pointer-events-none shadow rounded-lg appearance-none border text-gray-400 w-full  py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
-                            placeholder="123456"
+                            placeholder="88888888"
                         />
                     </div>
-                    <div className="mb-6 pl-10 pr-10">
+                    <div className="mb-6 lg:mb-4">
                         <label className="block text-gray-700 font-bold mb-2">
                             รหัสผ่านใหม่
                         </label>
                         <input
-                            className="shadow rounded-lg appearance-none border  w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            className="shadow rounded-lg appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         />
                     </div>
-                    <div className="flex items-center justify-between ml-10 mr-10">
+                    <div className="flex items-center justify-between">
+                    <div className="lg:ml-10 lg:mr-10">
                         <DialogEditpass isOpen={dialogOpen} onClose={handleDialogClose} buttonText={"บันทึก"} />
                         <button
                             onClick={() => {
                                 route.push("/user");
                             }}
                             type="submit"
-                            className="bg-red-600 rounded-full hover:bg-gray-500 text-white font-bold py-2 px-4  focus:outline-none focus:shadow-outline"
+                            className="bg-red-600 rounded-full hover:bg-gray-500 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline lg:ml-4"
                         >
                             ยกเลิก
                         </button>
+                    </div>
                     </div>
                 </form>
 

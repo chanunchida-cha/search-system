e075ce1f2689ff12948d3f6e56c7ba5f75898cc2
gulnwaot55 <aotzatingtong5@gmail.com>
2023-04-26 18:15:29 +0700

    EditPassword UI Min 4 Max 8

--- src/components/edit-password/EditPassword.tsx
+++ src/components/edit-password/EditPassword.tsx
@@ -6,32 +6,32 @@ import DialogEditpass from "~/utils/dialog_editpass";
 type Props = {};
 
 function EditPassword({ }: Props) {
-    const route = useRouter();
-    const [dialogOpen, setDialogOpen] = useState(false);
-    const [password, setPassword] = useState("")
-    const [disabled, setDisabled] = useState(true)
-    console.log(password);
-    
+  const route = useRouter();
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [password, setPassword] = useState("")
+  const [disabled, setDisabled] = useState(true)
+  console.log(password);
 
-    const handleDialogClose = () => {
-        setDialogOpen(false);
-    };
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault();
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+  }
+
+  const handleChangePassword = async (e: any) => {
+    setPassword(e.target.value)
+    if (e.target.value.length >= 4) {
+      setDisabled(false)
+    } else {
+      setDisabled(true)
     }
+  }
 
-    const handleChangePassword = async (e: any) => {
-        setPassword(e.target.value)
-        if (e.target.value.length >= 8) {
-          setDisabled(false)
-        } else {
-          setDisabled(true)
-        }
-      }
-
-    return (
-        <div className="flex items-center justify-center h-screen bg-white rounded-lg">
+  return (
+    <div className="flex items-center justify-center h-screen bg-white rounded-lg">
       <div className="w-11/12 md:w-1/2 lg:w-1/3">
         <form className="bg-gray-100 shadow-md rounded-lg px-8 pt-20 pb-20 lg:pl-20 lg:pr-20" onSubmit={handleSubmit}>
           <div className="mb-4 lg:mb-2">
@@ -44,16 +44,16 @@ function EditPassword({ }: Props) {
           </div>
           <div className="mb-6 lg:mb-4">
             <label className="block text-gray-700 font-bold mb-2">รหัสผ่านใหม่ <span className="text-red-500">*</span></label>
-            <input className="shadow rounded-lg appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value={password} onChange={handleChangePassword} required />
+            <input className="shadow rounded-lg appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value={password} maxLength={8} onChange={handleChangePassword} required />
           </div>
           <div className="flex items-center justify-between">
-            <DialogEditpass disabled={disabled}  isOpen={dialogOpen} onClose={handleDialogClose} buttonText={"บันทึก"} />
-            <button onClick={() => { route.push('/user') }} type="submit" className="bg-red-600 rounded-full hover:bg-gray-500 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline" >ยกเลิก</button>
+            <DialogEditpass disabled={disabled} isOpen={dialogOpen} onClose={handleDialogClose} buttonText={"บันทึก"} />
+            <button onClick={() => { route.push('/user') }} type="submit" className="bg-red-500 rounded-full hover:bg-gray-500 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline" >ยกเลิก</button>
           </div>
         </form>
       </div>
     </div>
-    );
+  );
 }
 
 export default EditPassword;
